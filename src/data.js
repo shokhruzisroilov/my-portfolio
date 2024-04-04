9e509d050689a8e7a6cd745cbfcedbacaad6c1eb
shokhruzisroilov <shokhruzisroilov@gmail.com>
2024-04-04 09:55:43 +0500

    dreams shop new portfolio

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
-import eCommerse from './assets/images/e-commerse-image.jpg'
+import dreamsShop from './assets/images/dreams-shop.jpg'
 import countryInfo from './assets/images/country-info-image.jpg'
 import todoApp from './assets/images/todo-app-image.jpg'
 import owaBooks from './assets/images/owa-books-image.jpg'
@@ -9,9 +9,17 @@ import zummaPharma from './assets/images/zumma-pharma.jpg'
 const ProjectCardData = [
 	{
 		id: uuidv4(),
+		imgsrc: dreamsShop,
+		title: 'Dreams Shop',
+		text: 'Dreams Shop online market',
+		view: 'https://dreams-shop.vercel.app/',
+		source: 'https://github.com/shokhruzisroilov/dream-shop-e-commerse.git',
+	},
+	{
+		id: uuidv4(),
 		imgsrc: excellanceTeam,
-		title: 'ExcelLanceTeam Website',
-		text: 'ExcelLanceTeam my team website',
+		title: 'ExcelLance Website',
+		text: 'ExcelLance my team website',
 		view: 'https://excellance-team.vercel.app/',
 		source: 'https://github.com/shokhruzisroilov/excellance-team.git',
 	},
@@ -21,15 +29,7 @@ const ProjectCardData = [
 		title: 'Owa Books Website',
 		text: 'Owa books fullstack crud website',
 		view: 'https://owa-books-frontend.vercel.app/',
-		source: 'https://github.com/shokhruzisroilov/owa-books.git',
-	},
-	{
-		id: uuidv4(),
-		imgsrc: eCommerse,
-		title: 'OrginalUz',
-		text: 'OrginalUz E-commerse website',
-		view: 'https://orginal-uz-e-commerse.vercel.app/',
-		source: 'https://github.com/shokhruzisroilov/OrginalUz_E-commerse.git',
+		source: 'https://github.com/shokhruzisroilov/owa-books-frontend.git',
 	},
 	{
 		id: uuidv4(),
