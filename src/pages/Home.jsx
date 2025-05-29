e986e92f5356743bbb991d50f139e2e100076085
Shohruz Isroilov <shokhruzisroilov@gmail.com>
2025-05-29 12:12:57 +0500

    Add Google site verification

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,46 @@
 import './Home.scss'
 
-import HomeTexts from '../components/homeTexts/HomeTexts'
+import { Helmet } from 'react-helmet'
 import HomeImage from '../components/homeImage/HomeImage'
+import HomeTexts from '../components/homeTexts/HomeTexts'
 import Social from '../components/social/Social'
 
 function Home() {
 	return (
-		<div className='home'>
-			<div className='home__container'>
-				<Social />
-				<HomeTexts />
-				<HomeImage />
+		<>
+			<Helmet>
+				<title>Shohruz Isroilov | Frontend Developer</title>
+				<meta
+					name='description'
+					content='React, HTML, CSS va JavaScript asosidagi portfolio. Amaliy loyihalar, aloqa va ish tajribasi.'
+				/>
+				<meta
+					name='keywords'
+					content='Frontend Developer, React, HTML, CSS, Portfolio, Shohruz Isroilov'
+				/>
+				<meta
+					property='og:image'
+					content='https://shokhruzisroilov.vercel.app/cover.jpg'
+				/>
+				<meta
+					property='og:title'
+					content='Shohruz Isroilov | Frontend Developer'
+				/>
+				<meta
+					property='og:description'
+					content='Portfolio sayt: React, Tailwind, HTML/CSS va JavaScript darslari'
+				/>
+				<meta property='og:url' content='https://shokhruzisroilov.vercel.app' />
+				<meta property='og:type' content='website' />
+			</Helmet>
+			<div className='home'>
+				<div className='home__container'>
+					<Social />
+					<HomeTexts />
+					<HomeImage />
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
